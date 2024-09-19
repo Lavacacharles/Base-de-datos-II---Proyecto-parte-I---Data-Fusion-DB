dc43fd6bc3e33ca5e3efcbf703ccf7acea947f0c
marcelino123455 <112722155+marcelino123455@users.noreply.github.com>
2024-09-19 15:23:48 -0500

    Add time execution trial

--- Interfaz/test2.py
+++ Interfaz/test2.py
@@ -1,5 +1,6 @@
 # import info
 import prueba 
+import time
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QTabWidget, QTableWidget, \
     QTableWidgetItem, QCheckBox, QTextEdit, QPushButton
@@ -29,7 +30,7 @@ class MiSGDB(QWidget):
         # Área de texto
         area_texto = QTextEdit()
         area_texto.setText(
-            "insert into table Order from file('C:\\data.csv')\nusing index hash;\n\nselect * from Order;")
+            "select * from")
         print(area_texto)
         
         # Botón Ejecutar
@@ -92,10 +93,12 @@ class MiSGDB(QWidget):
                 self.result_table.setItem(row, i, QTableWidgetItem(str(recordAtributos[i])))
 
         # Opciones para ejecutar
+        self.total = 0
         def ejecutar_comando():
             texto = area_texto.toPlainText() # Inserta lo que debe hacer 
             match texto:
                 case "select * from":
+                    start_time = time.time()
                     records = FixedFILE.readAll() 
                     self.result_table.clearContents()
                     num_rows = len(records)
@@ -104,18 +107,17 @@ class MiSGDB(QWidget):
                         recordAtributos = get_Lista_From_Char_Vector(record)
                         for i in range(len(recordAtributos)):
                             self.result_table.setItem(row, i, QTableWidgetItem(str(recordAtributos[i])))
-
+                    end_time = time.time()
+                    self.total = (end_time - start_time) * 1000
                 case "select ONE":
                     record = FixedFILE.readAll()[0]
                     self.result_table.clearContents()
                     self.result_table.setRowCount(1)
                     recordAtributos = get_Lista_From_Char_Vector(record)
                     for i in range(len(recordAtributos)):
-                            self.result_table.setItem(0, i, QTableWidgetItem(str(recordAtributos[i])))
+                        self.result_table.setItem(0, i, QTableWidgetItem(str(recordAtributos[i])))
 
-                    
-
-
+            self.footer_label2.setText(f"{self.total:.2f} milisec")
             print("Texto en el área de texto:", texto)
 
         # Conectar el botón a la función ejecutar_comando
@@ -129,12 +131,31 @@ class MiSGDB(QWidget):
         tabs.addTab(explain_tab, "Explain")
         tabs.addTab(transx_tab, "Transx")
 
+        # Footer
+        footer_layout = QHBoxLayout()
+        footer_label = QLabel('Time execution:')
+        self.footer_label2 = QLabel(str(self.total)+ " milisec")
+        self.footer_label2.setStyleSheet("""
+            QLabel {
+                background-color: lightblue;  /* Fondo azul claro */
+                border-radius: 10px;           /* Bordes redondeados */
+                padding: 5px 10px;            /* Espaciado interno */
+                color: black;                 /* Color del texto */
+            }
+        """)
+
+        # Añadir widgets al footer
+        footer_layout.addWidget(footer_label)
+        footer_layout.addWidget(self.footer_label2)
+
         # Añadir todos los layouts al layout principal
         layout_principal.addLayout(layout_superior)
         layout_principal.addWidget(tabs)
+        layout_principal.addLayout(footer_layout)
 
         # Establecer el layout de la ventana principal
         self.setLayout(layout_principal)
+        
 
 
 if __name__ == '__main__':
